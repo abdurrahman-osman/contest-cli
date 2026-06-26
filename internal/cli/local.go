e771597aac2abd8883077a0f5e8deba94d9fece3
abdurrahman-osman <abdurrahmanresmi@gmail.com>
2026-06-26 10:05:16 +0300

    update language to EN. remove unused comments.

--- internal/cli/local.go
+++ internal/cli/local.go
@@ -8,29 +8,24 @@ import (
 	"strings"
 )
 
-// runLocal "local" alt komutu çalıştırıldığında devreye girer.
+// runLocal "local" subcommand.
 func runLocal(args []string) {
-	// Sadece bu komuta özel bayrak kümesi
 	localCmd := flag.NewFlagSet("local", flag.ExitOnError)
 
-	// İhtiyacımız olan parametreler
 	targetFlag := localCmd.String("target", "", "Target IP ve Port (required) (örn: 192.168.1.10)")
 	portFlag := localCmd.String("port", "80", "Target Port (örn: 9092)(Default: 80)")
 	protoFlag := localCmd.String("proto", "tcp", "Protocol (tcp veya udp)")
 	timeoutFlag := localCmd.Duration("timeout", 3*time.Second, "Timeout Seconds")
 
-	// Kullanıcının girdiği verileri ayrıştırıyoruz
+
 	localCmd.Parse(args)
 
-	// Validasyon (Doğrulama): Hedef girilmemişse hata verip kullanımı gösteriyoruz.
 	if *targetFlag == "" {
 		fmt.Println("Error: --target flag is required!")
 		localCmd.Usage()
 		return
 	}
 
-	// Task 1'in sonu. Task 2'de buraya bağlantı kodlarını yazacağız.
-	fmt.Printf("[LOCAL TEST] %s üzerinden %s hedefine bağlanılacak...\n", *protoFlag, *targetFlag)
 
 	var addr string
 	if !strings.Contains(*targetFlag, ":") {
--- internal/cli/root.go
+++ internal/cli/root.go
@@ -6,28 +6,27 @@ import (
 	"os"
 )
 
-// Execute fonksiyonu uygulamamızın başlangıç noktasıdır.
+
 func Execute() {
-	// 1. Kendi özel yardım menümüzü tanımlıyoruz.
 	flag.Usage = func() {
 		fmt.Println("Contest CLI - Network Connection Testing")
 		fmt.Println("Usage: contest <command> [flags]")
 		fmt.Println("\nCommands:")
 		fmt.Println("  version    Show application version")
 		fmt.Println("\nFlags:")
-		flag.PrintDefaults() // Tanımladığımız tüm bayrakları (--help vs) ekrana basar
+		flag.PrintDefaults() 
 	}
 
-	// 2. İşletim sisteminden gelen bayrakları okur ve işler
+
 	flag.Parse()
 
-	// 3. Kullanıcı hiçbir alt komut (subcommand) girmediyse:
+
 	if len(flag.Args()) < 1 {
 		flag.Usage()
-		os.Exit(1) // Hata ile çıkış yap
+		os.Exit(1) 
 	}
 
-	// 4. Kendi komut yönlendiricimizi (Router) yazıyoruz:
+
 	command := flag.Args()[0]
 
 	switch command {
@@ -35,15 +34,12 @@ func Execute() {
 		fmt.Println("Contest CLI v1.0.0")
 		
 	case "local":
-		// 'local' komutunu çalıştır ve sonrasındaki tüm parametreleri ona gönder
 		runLocal(flag.Args()[1:])
 		
 	case "ssh":
-		// Henüz yazılmadı, ilerleyen tasklarda dolduracağız.
 		fmt.Println("SSH module will be added soon.")
 		
 	case "k8s":
-		// Henüz yazılmadı, ilerleyen tasklarda dolduracağız.
 		fmt.Println("Kubernetes module will be added soon.")
 		
 	default:
